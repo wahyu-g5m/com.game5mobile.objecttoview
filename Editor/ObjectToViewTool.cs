1cd584da67a8316a81dee3d57b8f30cf3e60f33a
wahyugam5mobile <wahyu@game5mobile>
2025-04-24 16:27:12 +0700

    init: initialize package

---
+++ Editor/ObjectToViewTool.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+
+public static class ObjectToViewTool
+{
+    [MenuItem("Tools/Move Object To View #&p")]
+    private static void BringToView()
+    {
+        var target = Selection.activeGameObject;
+
+        if (target == null) return;
+        var zPos = target.transform.position.z;
+
+        EditorApplication.ExecuteMenuItem("GameObject/Align With View");
+
+        var pos = target.transform.position;
+        pos = new Vector3(pos.x, pos.y, zPos);
+        target.transform.position = pos;
+    }
+}
